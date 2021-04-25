5de5c08c6e8391f30dc32fb91c9f5532dabbcc1e
ulziisuren <uhunt123@gmail.com>
2021-04-25 11:34:40 +0800

    hereglegchiin oruulsan utgiig avna

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-// Дэлгэцтэй ажиллах контроллер
+// Дэлгэцтэй ажиллах контроллер --------------------------------------------------------------------------------------------------------
 var uiController = (function () {
 
+    var DOMStrings = { // html - с утгуудыг барьж авч тусдаа нэг ОБ - д хийж байна
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+
+    };
+
+    return { // Энэ бол PUBLIC SERVICE юм
+        getInput: function () {
+            return {
+                // type: document.querySelector(".add__type").value, 
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMStrings;
+        }
+    }
 })();
 
-// Санхүүтэй ажиллах контроллер
+// Санхүүтэй ажиллах контроллер -----------------------------------------------------------------------------------------------------------
 var financeController = (function () {
 
 })();
 
-// Программын холбогч контроллер
+// Программын холбогч контроллер --------------------------------------------------------------------------------------------------------------------
 var appController = (function (uiController, financeController) {
+
+    var DOM = uiController.getDOMstrings();
 
     var ctrlAddItem = function () {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-        console.log("Дэлгэцээс өгөгдлөө олж авах хэсэг");
+        console.log(uiController.getInput())
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана. 
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана. 
     }
 
-    document.querySelector(".add__btn").addEventListener("click", function () {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
         ctrlAddItem();
     });
 
